9665aa3509e79acaad1e7db1cb61b05f89e7716c
GreedIsGood10000 <45040538+GreedIsGood10000@users.noreply.github.com>
2019-03-17 22:18:07 +0700

    Initial commit

---
+++ Data/EmployeeInfoWebAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeInfoWebApp.Models
+{
+    //Domain model
+    public class EmployeeInfoWebAppContext : DbContext
+    {
+        public EmployeeInfoWebAppContext (DbContextOptions<EmployeeInfoWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeInfoWebApp.Models.EmployeeInfo> EmployeeInfo { get; set; }
+    }
+}
---
+++ Pages/EmployeeInfoCreate.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using EmployeeInfoWebApp.Models;
+using EmployeeInfoWebApp.Repositories;
+
+namespace EmployeeInfoWebApp.Pages
+{
+    public class EmployeeInfoCreateModel : PageModel
+    {
+        private readonly EmployeeInfoRepository _repository;
+
+        public EmployeeInfoCreateModel(EmployeeInfoWebApp.Models.EmployeeInfoWebAppContext context)
+        {
+            _repository = new EmployeeInfoRepository(context);
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public EmployeeInfo EmployeeInfo { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //Application service
+            await _repository.Create(EmployeeInfo);
+
+            return Page();
+        }
+    }
+}
